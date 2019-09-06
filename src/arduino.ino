c7c300a35551cae78493bb4c508d52d7c82a0bab
Massinissa <hamidimassinissa@gmail.com>
2019-09-06 18:47:51 +0200

    Add files via upload

---
+++ src/arduino.ino
@@ -0,0 +1,37 @@
+
+//Librairie qui permet à la arduino de communiqué en série sur les broches 1 et 2
+#include <SoftwareSerial.h>
+//Définie notre broche de communication, ici la broche 0 noté D3 sur la arduino
+int PinAnalogiqueHumidite=0;   
+
+//notre variables d'humidité sol   
+int hsol;  
+
+//défini nos broches de communication RX et TX sur les broches D2 et D3 
+SoftwareSerial sw(2, 3); 
+
+void setup(){ 
+    Serial.begin(9600); 
+    pinMode(PinAnalogiqueHumidite, INPUT);     
+    Serial.begin(115200);
+    sw.begin(115200); 
+
+
+}
+
+void loop() { 
+    //lit la valeur du capteur
+    hsol = analogRead(PinAnalogiqueHumidite); 
+
+    //envoi la valeur du capteur
+    sw.print("\"hsolValue\":");
+    sw.print(hsol);//offset
+    sw.println();
+    
+    //défini la frequence d'envoi en miliseconde, soit ici 10sec
+    delay(10000);
+
+}
+
+
+
---
+++ src/esp32.ino
@@ -0,0 +1,75 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+//on définie les informations de connection wifi et serveur
+const char* ssid = "wifiPTR";
+const char* password = "mdpwifi.";
+const char* mqtt_server = "m23.cloudmqtt.com";
+#define mqtt_port 18528
+#define MQTT_USER "nodemcu"
+#define MQTT_PASSWORD "test"
+
+
+WiFiClient wifiClient;
+
+PubSubClient client(wifiClient);
+
+//connection wifi
+void setup_wifi() {
+  
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    //Permet d'initié la connection wifi
+    WiFi.begin(ssid, password);
+    //Attend que la connexion s'éffectue 
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+}
+
+void reconnect() {
+
+  while (!client.connected()) {
+    //défini notre nom sur le serveur
+    String clientId = "ESP32";
+    //permet d'inité la connectio au serveur MQTT
+    if (client.connect(clientId.c_str(),MQTT_USER,MQTT_PASSWORD)) {
+      Serial.println("connected");
+      //Si on arrive à se connecté on envoi un helloword sur le topic test
+      client.publish("/nodemcu/test", "hello world");
+    } else {
+      //On retry de ce connecté toutes les 5secondes
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(500);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  reconnect();
+}
+
+void publishSerialData(char *serialData){//cette fonction publie les données capteur sur le topic humidite_sol
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.publish("/nodemcu/humidite_sol", serialData);
+}
+void loop() {
+   client.loop();
+   if (Serial.available() > 0) {
+     char bfr[501];
+     memset(bfr,0, 501);
+     Serial.readBytesUntil( '\n',bfr,500);
+     publishSerialData(bfr);
+   }
+ }
